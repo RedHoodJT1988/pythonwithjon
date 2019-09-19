199895cdd61be99f04ab4d77f66cf61268b59110
RedHoodJT1988 <31198731+RedHoodJT1988@users.noreply.github.com>
2019-09-18 21:12:26 -0500

    added strings and comments files

---
+++ PythonBasics/comments.py
@@ -0,0 +1,18 @@
+
+greeting = "Hello World"
+print(greeting)
+
+# Addition = +
+# Subtraction = -
+# Multiplication = *
+# Division = /
+
+# This function below rounds the value of the quotient of two numbers
+
+
+def numbers(x, y):
+    return round(x / y)
+
+
+print(numbers(2.36, 36.8))
+print(2.36 / 36.8)
---
+++ PythonBasics/strings.py
@@ -0,0 +1,23 @@
+# double_quotes = "I'm a string in double quotes."
+
+# single_quotes = 'I\'m a string in single quotes.'
+
+# quoted_text = 'And he said \'I cannot believe its not butter!\' I can\'t'
+
+# print(double_quotes)
+# print(single_quotes)
+# print(quoted_text)
+
+name = "jason todd"
+print(name.title())
+
+email = "JasonTODDRocks@batmansnoozes.com"
+print(email.lower())
+
+print(name.capitalize())
+
+print(name.isascii())
+
+print(name.format())
+
+print(name)
