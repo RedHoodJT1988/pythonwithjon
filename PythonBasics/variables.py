79a708555a9d0009e860ca7eeca9a67761e14314
RedHoodJT1988 <31198731+RedHoodJT1988@users.noreply.github.com>
2019-09-18 20:15:57 -0500

    Added PythonBasics folder to project and variables.py

---
+++ PythonBasics/variables.py
@@ -0,0 +1,10 @@
+# Using the variables with the print method will make
+# The below text much easier.
+print("Hello, world")
+print("Hello, my name is Jon. I am a Python developer.")
+
+# Here is where we start to use variables
+greeting = "Hello, "
+firstName = "Jonathan"  # Here you can input your own name to see what happens
+
+print(greeting + " " + firstName + "!")
