20e210bb52bc81a8dfd751de9345dc128de2bdda
RedHoodJT1988 <31198731+RedHoodJT1988@users.noreply.github.com>
2019-11-10 13:26:29 -0600

    added tuples.py file

---
+++ AdvancedPythonBasics/tuples.py
@@ -0,0 +1,9 @@
+empty_tuple = ()
+print('empty tuple: ', type(empty_tuple))
+
+single_tuple = (1, )  # (1) Python sees this as an int
+print('single tuple: ', type(single_tuple))
+
+character = ("Zero", 100, "Z-Sword", 100)
+print(character)
+print('character tuple: ', type(character))
