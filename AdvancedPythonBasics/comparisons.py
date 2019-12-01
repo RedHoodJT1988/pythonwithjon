2a8828fe01044852fdd7c6ccc5fd039bcf7b7b71
RedHoodJT1988 <31198731+RedHoodJT1988@users.noreply.github.com>
2019-12-01 13:46:38 -0600

    add comparisons file

---
+++ AdvancedPythonBasics/comparisons.py
@@ -0,0 +1,37 @@
+# Comparing basic data types
+# a = 1
+# b = 1
+# print(a == b)  # this means equal to
+# print(a != b)  # != this means not equal
+# a = "Ryu"
+# b = "Ryu"
+# print(a == b)
+# print(a != b)
+
+# Comparing container types
+# a = ["Ryu", "Ken", "Vega"]
+# b = ["Ryu", "Ken", "Vega"]
+# print(a == b)
+# print(a != b)
+
+# is keyword examples
+# a = True
+# print(a is True)
+
+# a = [1, 2, 3]
+# b = [1, 2, 3]
+# print(a == b)  # True
+# print(a is b) # not the same. Returns False.
+# print(b is a)
+
+# Comparing numbers
+# print(1 < 10)
+# print(20 <= 20)
+# print(10 > -10)
+# print(-11 < 0)
+# print(35 >= 30)
+# print(35 < 30)
+
+# Comparing strings and letters
+print('A' > 'a')
+print("string" < "Hello")
