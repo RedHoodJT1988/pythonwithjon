41db14fc1a517b7813e6019267449a87843810bb
RedHoodJT1988 <31198731+RedHoodJT1988@users.noreply.github.com>
2019-12-02 19:51:05 -0600

    upated whileloop file

--- AdvancedPythonBasics/whileloop.py
+++ AdvancedPythonBasics/whileloop.py
@@ -1,6 +1,12 @@
 # While Loop example
+# Non infinite loop
 counter = 0
-maxCount = 4
+maxCount = 5
 
 while counter < maxCount:
-    print(f"The count is: {counter}")
+    counter = counter + 1
+    print(counter)
+
+# DO NOT UNCOMMENT OUT THIS CODE **** This is an infinite loop example ****
+# while counter < maxCount:
+#     print(f"The count is: {counter}")
