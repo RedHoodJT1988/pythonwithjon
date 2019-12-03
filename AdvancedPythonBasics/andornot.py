b2ce0433c95e108b264bdd65c32766327cd33506
RedHoodJT1988 <31198731+RedHoodJT1988@users.noreply.github.com>
2019-12-02 19:16:03 -0600

    add andornot file

---
+++ AdvancedPythonBasics/andornot.py
@@ -0,0 +1,20 @@
+# Boolean operators
+
+# the "AND" operator
+
+a = True
+b = False
+# print(a and b)  # If both a and b or True then it prints True
+
+c = False
+d = False
+# print(c and d)  # If one of these values returns False then it prints False
+
+# the "OR" operator
+e = False
+f = False
+# print(e or f)  # If either of these values returns True then it prints True
+# print(a and (b or c or d or e or f))
+
+# the "NOT" operator
+print(not b)
