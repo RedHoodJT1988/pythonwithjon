9a0b3b5bde8de4fdbd1b5a07b1efb7566912da30
RedHoodJT1988 <31198731+RedHoodJT1988@users.noreply.github.com>
2020-01-02 21:13:54 -0600

    initial text-adventure game code

---
+++ Text-Adventure/echo.py
@@ -0,0 +1,8 @@
+# print("Type your name and press enter \n")
+name = input("Type your name and press enter \n Enter name: ")
+characterClass = input("Please enter your class: ")
+print("\n")
+print("name: ", name)
+print("Class: ", characterClass)
+
+
---
+++ Text-Adventure/game-notes.md
@@ -0,0 +1,4 @@
+setting: Washington, DC
+environment: War Torn, Apocalyptic
+race: Elf, Wizard, Warrior, Valkrie
+sex: male or female
