fa6e85a5fbaf6495ac33b67311db3178fb51cc49
RedHoodJT1988 <31198731+RedHoodJT1988@users.noreply.github.com>
2020-01-26 21:09:07 -0600

    added weapon classes and objects

--- Text-Adventure/escapefromdc.py
+++ Text-Adventure/escapefromdc.py
@@ -1,5 +1,43 @@
 # This program is a text-adventure game where your main goal is to Escape
 
+# Weapon Classes
+
+
+class Weapon:
+    # defining the str function to return the name
+    def __str__(self):
+        return self.name
+
+
+class Rock(Weapon):
+    def __init__(self):
+        self.name = "Rock"
+        self.description = "A hard object made of stone, used for bludegoning."
+        self.damage = 5
+
+
+class Knife(Weapon):
+    def __init__(self):
+        self.name = "Knife"
+        self.description = "A nice, small and sharp blade. Can be used for skinning as well as defense."
+        self.damage = 10
+
+
+class Bow(Weapon):
+    def __int__(self):
+        self.name = "Bow"
+        self.description = "A wooden stick with nylon attached for shooting from long range."
+        self.damage = 12
+        self.arrow = 0
+
+
+class Handgun(Weapon):
+    def __init__(self):
+        self.name = "Handgun"
+        self.description = "A pistol. Needs bullets, but is good for defense and hunting."
+        self.bullets = 0
+        self.damage = 30
+
 
 def get_player_command():
     return input("Input direction: ")
@@ -18,7 +56,8 @@ def play():
     ''')
     # Initialize and set variables
     action_input = get_player_command()
-    inventory = ['Handgun', 'bullets', 'stale bread', 'medpack']
+    inventory = [Rock(), 'bullets', 'stale bread', 'medpack']
+    items = ["apple", "shotgun shells", "shotgun"]
 
     # Handle player commands
     if action_input in ['n', 'N']:
@@ -32,6 +71,10 @@ def play():
     elif action_input in ['i', 'I']:
         print("Inventory: ")
         print(inventory)
+    elif action_input in ['l', 'L']:
+        print("Searching area...")
+        print("Found items: ")
+        print(items[2])
     else:
         print("Invalid direction! Please type a valid direction.")
 
